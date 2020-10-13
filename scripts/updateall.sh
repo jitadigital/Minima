999e7f052b4660a05b11953433bb1304b46391db
Jérôme Rousselot <jeromerousselot@gmail.com>
2020-10-13 18:55:05 +0100

    Revert "negative array size catch error"
    This reverts commit c4eba715135a48f8ed848ddc70be36d0bd920ee5.

--- scripts/updateall.sh
+++ scripts/updateall.sh
@@ -6,24 +6,19 @@
 ./update.sh minima-private-2-vm --zone=europe-west4-a private2
 #sleep 300
 
-#Run through the public servers..
-#./update.sh minima-public-1-vm --zone=europe-west4-a public
-#sleep 60
-#./update.sh minima-public-2-vm --zone=europe-west4-a public
-#sleep 60
-#./update.sh minima-public-3-vm --zone=europe-west4-a public
-#sleep 60
-#./update.sh minima-public-4-vm --zone=europe-west4-a public
-#sleep 60
-#./update.sh minima-public-5-vm --zone=europe-west4-a public
-#sleep 60
-#./update.sh minima-public-6-vm --zone=europe-west4-a public
-#sleep 60
-
 #Update the miner..
-#./update.sh minima-miner-1-vm --zone=europe-west2-a miner
+#./update.sh minima-miner-1-vm miner
 #sleep 300
-#./update.sh minima-miner-2-vm --zone=europe-west2-a miner
+#./update.sh "minima-miner-2-vm --zone=europe-west2-a" miner
 
+#Run through the public servers..
+#./update.sh minima-public-1-vm public
+#sleep 60
+#./update.sh minima-public-2-vm public
+#sleep 60
+#./update.sh minima-public-3-vm public
+#sleep 60
+#./update.sh minima-public-4-vm public
+#sleep 60
 
 
