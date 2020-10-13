70c845c7514b66df8e007e02e501abb3cdfe041c
Jérôme Rousselot <jeromerousselot@gmail.com>
2020-10-13 18:55:05 +0100

    Revert "blocks added to tree after init"
    This reverts commit 41830157a9f05056e8e6a99d58d0909d818f6d66.

--- src/org/minima/system/brains/ConsensusBackup.java
+++ src/org/minima/system/brains/ConsensusBackup.java
@@ -230,15 +230,6 @@ public class ConsensusBackup extends ConsensusProcessor {
 						trow.setOnChainBlock(false);
 						trow.setIsInBlock(true);
 						trow.setInBlockNumber(block);
-						
-						//Is it a block ?
-						TxPoW tpow = trow.getTxPOW();
-						if(tpow.isBlock()) {
-							//Add all the children
-							if(getMainDB().getMainTree().addNode(new BlockTreeNode(tpow))) {
-								getMainDB().addTreeChildren(tpow.getTxPowID());
-							}
-						}
 					}
 				}
 			}
