b0d2542c4b08bedaac050ded51f066192be28dfc
Jérôme Rousselot <jeromerousselot@gmail.com>
2020-10-13 18:55:05 +0100

    Revert "0.96.12 - fix 0.5 second speed BUG!"
    This reverts commit c22a68898bf17fdfe48715685e3922a0738921e2.

--- src/org/minima/database/txpowtree/BlockTree.java
+++ src/org/minima/database/txpowtree/BlockTree.java
@@ -88,7 +88,7 @@ public class BlockTree {
 	 * @return
 	 */
 	public boolean addNode(BlockTreeNode zNode) {
-		//Do we have it allready.. 
+		//Do we have it allready.. DOUBLE check as this done already
 		BlockTreeNode exists = findNode(zNode.getTxPowID());
 		if(exists != null) {
 			//All ready in there..
