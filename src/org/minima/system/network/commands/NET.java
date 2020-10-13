8af1279353879f3b04d6791a1c392ef91ff221eb
Jérôme Rousselot <jeromerousselot@gmail.com>
2020-10-13 18:55:05 +0100

    Revert "Adding disconnect"
    This reverts commit 4f826e6f8edc9f9134a5e4e13d6c33ed08cb4857.

--- src/org/minima/system/network/commands/NET.java
+++ src/org/minima/system/network/commands/NET.java
@@ -107,11 +107,11 @@ public class NET implements Runnable {
 			comms.PostMessage(stopper);
 		
 		}else if(mCommand.startsWith("disconnect ")) {
-			String uid = strtok.nextToken();
+			String hostport = strtok.nextToken();
 			
 			//Stop this Comms Server
-			Message stopper = new Message(CommsManager.COMMS_DISCONNECT);
-			stopper.addString("uid", uid);
+			Message stopper = new Message(CommsManager.COMMS_CONNECT);
+			stopper.addString("hostport", hostport);
 			stopper.addString("minidappid", mMiniDAPPID);
 			
 			//Post it..
