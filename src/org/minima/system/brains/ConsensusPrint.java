771987854c1eb276271eca54f0eb3d8ed111f2f5
Jérôme Rousselot <jeromerousselot@gmail.com>
2020-10-13 18:55:05 +0100

    Revert "new parameters. Check blocktime correct.."
    This reverts commit adb79a9bf63c04620f9e752075d0b3c27a643477.

--- src/org/minima/system/brains/ConsensusPrint.java
+++ src/org/minima/system/brains/ConsensusPrint.java
@@ -947,6 +947,7 @@ public class ConsensusPrint extends ConsensusProcessor {
 			
 			status.put("lastblock", tip.getTxPow().getBlockNumber().toString());
 			status.put("lasttime", new Date(new Long(tip.getTxPow().getTimeMilli()+"")).toString());
+			
 			status.put("cascade", getMainDB().getMainTree().getCascadeNode().getTxPow().getBlockNumber().toString());
 			
 			status.put("difficulty", tip.getTxPow().getBlockDifficulty().to0xString());
@@ -975,9 +976,9 @@ public class ConsensusPrint extends ConsensusProcessor {
 			status.put("mempooltxn", unused.size());
 			status.put("mempoolcoins", getMainDB().getMempoolCoins().size());
 			
-			//The block used for speed calculation..
-			status.put("chainspeed", getMainDB().getMainTree().getChainSpeed());
+			//Chain details..
 			status.put("chainlength", getMainDB().getMainTree().getAsList().size());
+			status.put("chainspeed", getMainDB().getMainTree().getChainSpeed());
 			status.put("chainweight", root.getTotalWeight().toString());
 			
 			//Use the cached Version  - this is a slow operation
