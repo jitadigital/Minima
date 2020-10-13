1c92f8708ee031dd692e1cf1b55c37fd5f142ba5
Jérôme Rousselot <jeromerousselot@gmail.com>
2020-10-13 18:55:05 +0100

    Revert "first push"
    This reverts commit 22c4b3926d7fc88d23dfba907cf1a7ec52c2a7ca.

--- src/org/minima/system/network/minidapps/minilib/MinimaJSHandler.java
+++ src/org/minima/system/network/minidapps/minilib/MinimaJSHandler.java
@@ -1,4 +1,4 @@
-package org.minima.system.network.minidapps.minibackend;
+package org.minima.system.network.minidapps.minilib;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,10 +13,6 @@ import org.minima.system.network.commands.CMD;
 import org.minima.system.network.commands.FILE;
 import org.minima.system.network.commands.NET;
 import org.minima.system.network.commands.SQL;
-import org.minima.system.network.minidapps.minilib.MiniLibUtility;
-import org.minima.utils.MinimaLogger;
-import org.minima.utils.json.JSONObject;
-import org.mozilla.javascript.Function;
 
 /**
  * This class handles a single request then exits
@@ -24,43 +20,25 @@ import org.mozilla.javascript.Function;
  * @author spartacusrex
  *
  */
-public class MinimaJSHandler {
+public class MinimaJSHandler implements Runnable {
 
 	/**
-	 * JS BACKEND link
+	 * The Net Socket
 	 */
-	private BackEndDAPP mBackBone;
-	
+	Socket mSocket;
 	
 	/**
-	 * Main Constructor
+	 * Main COnstructor
 	 * @param zSocket
 	 */
-	public MinimaJSHandler(BackEndDAPP zBackBone) {
-		mBackBone = zBackBone;
+	public MinimaJSHandler(Socket zSocket) {
+		//Store..
+		mSocket = zSocket;
 	}
 
-	public void post(String zType, String zData, Function zCallback) {
-		
-		MinimaLogger.log("BackEnd Handle : "+zType+" "+zData);
-		
-		JSONObject res = new JSONObject();
-		res.put("type", "answer");
-		res.put("value", 43);
-		
-		//The response returned..
-		String finalResult = res.toString();
-		
-		//Create a native JSON
-		Object json = MiniLibUtility.makeJSONObject(finalResult, mBackBone.getContext(), mBackBone.getScope());
-		
-		//Make a function variable list
-		Object functionArgs[] = { json };
-	    
-		//Call the function..
-		zCallback.call(mBackBone.getContext(), mBackBone.getScope(), mBackBone.getScope(), functionArgs);
-	
-		/*// we manage our particular client connection
+	@Override
+	public void run() {
+		// we manage our particular client connection
 		BufferedReader in 	 		 	= null; 
 		PrintWriter out 	 			= null; 
 		
@@ -224,8 +202,6 @@ public class MinimaJSHandler {
 			} catch (Exception e) {
 				System.err.println("Error closing stream : " + e.getMessage());
 			} 	
-		}
-		
-		*/
+		}	
 	}
 }
