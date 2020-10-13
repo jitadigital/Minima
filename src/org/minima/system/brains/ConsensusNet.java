29aa0d5eacb53d5be044aa993aec50899cad1ae5
Jérôme Rousselot <jeromerousselot@gmail.com>
2020-10-13 18:55:05 +0100

    Revert "TxPoW gateway logic rejig"
    This reverts commit 252b47ee7e625cca55536d8fa3ee70cd3e84ccf3.

--- src/org/minima/system/brains/ConsensusNet.java
+++ src/org/minima/system/brains/ConsensusNet.java
@@ -503,35 +503,24 @@ public class ConsensusNet extends ConsensusProcessor {
 				//And remove the link..
 				getNetworkHandler().removeRequestedTxPow(txpowid);
 				
-				//Just return as this is already in a valid block - no more processing to do
 				return;
-			}
 			
-			//Was it requested anyway.. ?
-			boolean requested = false;
-			if(getNetworkHandler().isRequestedTxPow(txpowid)) {
-				requested = true;
-			}
+				//Is it a regular requested
+			}else if(getNetworkHandler().isRequestedTxPow(txpowid)) {
+				//Requested by you.. gets a pass..
+				getNetworkHandler().removeRequestedTxPow(txpowid);
 			
-			//Check the Validity..
-			boolean txnok = TxPoWChecker.checkTransactionMMR(txpow, getMainDB());
-			if(!txnok) {
-				//Was it requested.. ?
-				if(requested) {
-					//Ok - could be from a different branch block.. 
-					MinimaLogger.log("WARNING NET Invalid TXPOW (Requested..) : "+txpow.getBlockNumber()+" "+txpow.getTxPowID()); 
-				}else {
+			}else {
+				//Check the Validity..
+				boolean txnok = TxPoWChecker.checkTransactionMMR(txpow, getMainDB());
+				
+				if(!txnok) {
 					//Not requested invalid transaction..
-					MinimaLogger.log("ERROR NET Invalid TXPOW (UN-Requested..) : "+txpow.getBlockNumber()+" "+txpow.getTxPowID()); 
+					MinimaLogger.log("ERROR NET Invalid TXPOW (unrequested..) : "+txpow.getBlockNumber()+" "+txpow.getTxPowID()); 
 					return;	
 				}
 			}
-			
-			//Remove it from the list
-			if(requested) {
-				getNetworkHandler().removeRequestedTxPow(txpowid);
-			}
-		
+
 			/**
 			 * IT PASSES!
 			 * 
