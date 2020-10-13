b748c09f20a22365cb6a0fc43f7b094792bb1048
Jérôme Rousselot <jeromerousselot@gmail.com>
2020-10-13 18:55:05 +0100

    Revert "fix bug.. missing txpowid"
    This reverts commit c5ee417ec895d4179f2fb3ddf00f6810902b21ac.

--- src/org/minima/system/network/MinimaClient.java
+++ src/org/minima/system/network/MinimaClient.java
@@ -266,10 +266,7 @@ public class MinimaClient extends MessageProcessor {
 			//sendMessage(MinimaReader.NETMESSAGE_TXPOWID, txpowid);
 		
 			//Post it with a random delay..
-			TimerMessage txpowid = new TimerMessage(mRand.nextInt(2000), NETCLIENT_POSTTXPOWID);
-			txpowid.addObject("txpowid", zMessage.getObject("txpowid"));
-			
-			PostTimerMessage(txpowid);
+			PostTimerMessage(new TimerMessage(mRand.nextInt(2000), NETCLIENT_POSTTXPOWID));
 			
 		}else if(zMessage.isMessageType(NETCLIENT_POSTTXPOWID)) {
 			MiniData txpowid = (MiniData)zMessage.getObject("txpowid");
